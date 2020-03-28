66923b998a720cdc0939f83b3c142502f82c36ba
runze wang <wrz7788597918@gmail.com>
2020-03-27 21:54:04 -0700

    test

--- hello.java
+++ hello.java
@@ -1,8 +1,10 @@
-@RestController
-public class Hello {
 
-    @RequestMapping("/")
-    public String sayHello(){
-        return "abcdefg. Hello jenkins, " + new Date();
-    }
+public class Hello{
+	
+	public static void main(String[] args){
+		
+		for(int i=1;i<=10;i++)
+			System.out.println("Hello World " + i);
+	}
+	
 }
