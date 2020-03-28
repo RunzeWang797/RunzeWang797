294f84689be709303ccdf0a320155bda2593e74a
runze wang <wrz7788597918@gmail.com>
2020-03-27 21:57:14 -0700

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 public class Hello{
 	
 	public static void main(String[] args){
@@ -9,10 +8,3 @@ public class Hello{
 	}
 	
 }
-=======
-    @RequestMapping("/")
-    public String sayHello(){
-        return "abcdefg. Hello jenkins-test111, " + new Date();
-    }
-}
->>>>>>> 524e36165a5ac9a633e49155d226fa1494c24363
